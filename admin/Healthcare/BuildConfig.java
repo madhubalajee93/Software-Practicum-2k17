5647efc67da8f359ea6f22ecd5551b3f389e8a18
madhubalajee93 <bmadhavan@regis.edu>
2017-12-04 15:29:48 -0700

    Add files via upload

---
+++ admin/Healthcare/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.smart_health_prediction_doc;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
